72a5bbeb8024db5a55f0cc7ddbb051261369e96f
Augusto Collerone <collerone.augusto@gmail.com>
2024-08-23 21:07:43 -0300

    Gelato library & Hardhat config

--- src/contracts/hardhat.config.ts
+++ src/contracts/hardhat.config.ts
@@ -1,8 +1,45 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import { HardhatUserConfig } from "hardhat/config";
+
 import "@nomicfoundation/hardhat-toolbox";
+
+const providerApiKey =
+  process.env.ALCHEMY_API_KEY || "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
+// If not set, it uses the hardhat account 0 private key.
+const deployerPrivateKey =
+  process.env.DEPLOYER_PRIVATE_KEY ??
+  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+
+// If not set, it uses ours Etherscan default API key.
+const etherscanApiKey =
+  process.env.ETHERSCAN_API_KEY || "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      mining: {
+        interval: 1000,
+      },
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey],
+    },
+  },
+  defaultNetwork: "localhost",
+  etherscan: {
+    apiKey: `${etherscanApiKey}`,
+  },
+  ignition: {
+    requiredConfirmations: 1,
+    // strategyConfig: {
+    //   create2: {
+    //     salt: "0x0000000000000000000000000000000000000000000000000000000000000000",
+    //   },
+    // },
+  },
 };
 
 export default config;
