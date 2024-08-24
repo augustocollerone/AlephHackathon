62557b91b2116c6a1c3d2ca5a157e3f4a833a57d
Augusto Collerone <collerone.augusto@gmail.com>
2024-08-24 00:08:15 -0300

    Functions cleanup

--- src/contracts/contracts/MagicDCA.sol
+++ src/contracts/contracts/MagicDCA.sol
@@ -12,50 +12,27 @@ struct DcaTask {
     uint256 count;
     uint256 maxCount;
     address feeToken;
+    bytes32 gelatoTaskId; // Store the Gelato task ID directly in the task
 }
 
 contract MagicDCA is AutomateTaskCreator {
-    // Create a mapping nest of multiple tasksIds for each address
     mapping(address => DcaTask[]) public dcaTasks;
-    mapping(bytes32 => uint256) public taskIds;
-
-    bytes32 public taskId;
-
-    event CounterTaskCreated(bytes32 taskId);
 
     uint128 public constant INTERVAL = 3 minutes;
 
     event DcaTaskCreated(address indexed user, uint256 taskId, string name);
     event DcaTaskUpdated(address indexed user, uint256 taskId, string name);
     event DcaTaskDeleted(address indexed user, uint256 taskId);
-    event DcaTaskExecuted(address indexed user, uint256 taskId);
+    event DcaTaskExecuted(
+        address indexed user,
+        uint256 taskId,
+        uint256 amount,
+        address feeToken
+    );
 
     constructor(address payable _automate) AutomateTaskCreator(_automate) {}
 
-    function createTask() external payable {
-        require(taskId == bytes32(""), "Already started task");
-
-        bytes memory execData = abi.encodeCall(this.executeDcaTask, (1));
-
-        ModuleData memory moduleData = ModuleData({
-            modules: new Module[](2),
-            args: new bytes[](2)
-        });
-        // Ensure that the modules are in ascending order by their enum value
-        moduleData.modules[0] = Module.PROXY; // PROXY should come first
-        moduleData.modules[1] = Module.TRIGGER; // TRIGGER should come second
-
-        moduleData.args[0] = _proxyModuleArg(); // Corresponding to PROXY
-        moduleData.args[1] = _timeTriggerModuleArg(
-            uint128(block.timestamp),
-            INTERVAL
-        ); // Corresponding to TRIGGER
-
-        bytes32 id = _createTask(address(this), execData, moduleData, ETH);
-
-        taskId = id;
-        emit CounterTaskCreated(id);
-    }
+    function receive() external payable {}
 
     function createDcaTask(
         string memory _name,
@@ -64,21 +41,22 @@ contract MagicDCA is AutomateTaskCreator {
         uint256 _maxCount,
         address _feeToken
     ) external {
+        // Generate a random ID based on the block timestamp, the user's address, and the current task length
+        uint256 taskId = 1234567890; // TODO: Generate a random ID
+
         DcaTask memory newTask = DcaTask({
-            id: _id,
+            id: taskId,
             name: _name,
             amount: _amount,
             interval: _interval,
             lastExecuted: block.timestamp,
             count: 0,
             maxCount: _maxCount,
-            feeToken: _feeToken
+            feeToken: _feeToken,
+            gelatoTaskId: bytes32(0) // Initialize as empty, will set after creating Gelato task
         });
 
         dcaTasks[msg.sender].push(newTask);
-
-        // Create new id
-        uint256 newId = dcaTasks[msg.sender].length - 1;
 
         bytes memory execData = abi.encodeCall(
             this.executeDcaTask,
@@ -96,7 +74,7 @@ contract MagicDCA is AutomateTaskCreator {
         moduleData.args[0] = _proxyModuleArg(); // Corresponding to PROXY
         moduleData.args[1] = _timeTriggerModuleArg(
             uint128(block.timestamp),
-            INTERVAL
+            _interval
         ); // Corresponding to TRIGGER
 
         bytes32 gelatoTaskId = _createTask(
@@ -106,31 +84,20 @@ contract MagicDCA is AutomateTaskCreator {
             newTask.feeToken
         );
 
-        taskIds[gelatoTaskId] = newTask.id;
+        // Update the Gelato task ID in the new task
+        dcaTasks[msg.sender][dcaTasks[msg.sender].length - 1]
+            .gelatoTaskId = gelatoTaskId;
 
-        emit DcaTaskCreated(msg.sender, dcaTasks[msg.sender].length - 1, _name);
+        emit DcaTaskCreated(msg.sender, taskId, _name);
     }
 
-    function deleteDcaTask(uint256 _taskId) external {
-        require(_taskId < dcaTasks[msg.sender].length, "Invalid task ID");
-
-        uint256 lastTaskId = dcaTasks[msg.sender].length - 1;
-
-        // If the task to be deleted is not the last one, replace it with the last task
-        if (_taskId != lastTaskId) {
-            dcaTasks[msg.sender][_taskId] = dcaTasks[msg.sender][lastTaskId];
-        }
-
-        dcaTasks[msg.sender].pop(); // Remove the last element
-
-        emit DcaTaskDeleted(msg.sender, _taskId);
-    }
+    function deleteDcaTask(uint256 _taskId) external {}
 
     function getDcaTasks() external view returns (DcaTask[] memory) {
         return dcaTasks[msg.sender];
     }
 
-    function executeDcaTask(uint256 _id) public onlyDedicatedMsgSender {
+    function executeDcaTask(uint256 _id) external onlyDedicatedMsgSender {
         // uint256 newCount = count + _amount;
         // if (newCount >= MAX_COUNT) {
         //     _cancelTask(taskId);
@@ -141,8 +108,9 @@ contract MagicDCA is AutomateTaskCreator {
         // }
 
         (uint256 fee, address feeToken) = _getFeeDetails();
+
         _transfer(fee, feeToken);
 
-        emit DcaTaskExecuted(msg.sender, _id);
+        emit DcaTaskExecuted(msg.sender, _id, fee, feeToken);
     }
 }
