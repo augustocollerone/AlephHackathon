151683b03c06233f296feffc0f47cb4b4c0af626
Augusto Collerone <collerone.augusto@gmail.com>
2024-08-25 06:24:44 -0300

    Update createScheduleTransactions.js

--- src/contracts/createScheduleTransactions.js
+++ src/contracts/createScheduleTransactions.js
@@ -1,23 +1,21 @@
 const { Defender } = require("@openzeppelin/defender-sdk");
 
 exports.handler = async function (payload) {
-  console.log(payload.request.body);
+  const data = payload.request.body;
+  console.log(data);
 
   const creds = {
     apiKey: "RdxcMWTQotk8TuRQtyz55NaxuotkwyZi",
     apiSecret:
       "2gi4R16ViNTy2NzH4CqSyMSqx4cnAQMSp9YjFtjFfJeDv4knbcHSB3HzLpMdpe58",
-    //optional https config to keep connection alive. You can pass any configs that are accepted by https.Agent
-    httpsAgent: https.Agent({ keepAlive: true }),
   };
   const client = new Defender(creds);
 
-  const conditionRequest = payload.request.body;
-  const matches = [];
-  const eventHash = conditionRequest.hash;
-
-  // Inline code for the scheduled action
-  const codeToRun = `
+  data.matchReasons.forEach(async (reason) => {
+    const { user, taskId, name, amount, interval } = reason.params;
+    console.log(reason.params);
+    // Inline code for the scheduled action
+    const codeToRun = `
   const { Defender } = require('@openzeppelin/defender-sdk');
   const { ethers } = require('ethers');
 
@@ -37,31 +35,26 @@ exports.handler = async function (payload) {
   };
 `;
 
-  const myScheduledAction = {
-    name: `Scheduled-Action-For-Task`,
-    encodedZippedCode: Buffer.from(codeToRun).toString("base64"), // Encode the inline code
-    trigger: {
-      type: "schedule",
-      frequencyMinutes: 60, // Set the frequency as needed
-    },
-    paused: false,
-    environmentVariables: {
-      CONTRACT_ADDRESS: eventHash,
-    },
-  };
+    const myScheduledAction = {
+      name: `Scheduled-Action-For-Task`,
+      encodedZippedCode: Buffer.from(codeToRun).toString("base64"), // Encode the inline code
+      trigger: {
+        type: "schedule",
+        frequencyMinutes: 60, // Set the frequency as needed
+      },
+      paused: false,
+      environmentVariables: {
+        CONTRACT_ADDRESS: matchReasons.address,
+      },
+    };
 
-  const createdAction = await client.action.create(myScheduledAction);
-  console.log(`Scheduled action created: ${createdAction.name}`);
-
-  // metadata can be any JSON-marshalable object (or undefined)
-  //   matches.push({
-  //     hash: event.hash,
-  //     metadata: {
-  //       id: "customId",
-  //       timestamp: new Date().getTime(),
-  //       numberVal: 5,
-  //       nested: { example: { here: true } },
-  //     },
-  //   });
-  //   return { matches };
+    try {
+      const createdAction = await client.createScheduledAction(
+        myScheduledAction
+      );
+      console.log(`Scheduled action created: ${createdAction.name}`);
+    } catch (error) {
+      console.log(`Error creating scheduled action: ${error}`);
+    }
+  });
 };
